d6ee1e98c30a33f5a741834f517608e3f20cc9b1
ldayalag-tech <ldayalag@gmail.com>
2026-07-22 15:46:09 -0500

    Update calculadora.py
    AGREGAR LA MULTIPLICACIÓN
    LIZ AYALA

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,26 @@
-## CALCULADORA - VERSIÓN 2
-# Autor(a): liz ayala
+# CALCULADORA - VERSIÓN 3
+# Autor(a): Liz Ayala
 # ===========================================
+
+print("CALCULADORA")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-print("\n 1.Suma")
-print( 2.Restar")
+print("\n 1. Sumar")
+print(" 2. Restar")
+print(" 3. Multiplicar")
 
-opción = input("\n Seleccione una opción: ")
+opcion = input("\n Seleccione una opción: ")
 
-if opción == "1":
-  print("\n La suma es:", num1 + num2)
+if opcion == "1":
+    print("\n La suma es:", num1 + num2)
 
-elif opción == "2":
-  print("\n La resta es :", num1 - num2)
+elif opcion == "2":
+    print("\n La resta es:", num1 - num2)
+
+elif opcion == "3":
+    print("\n La multiplicación es:", num1 * num2)
 
 else:
-  print("\n Opción no valida.")
+    print("\n Opción no válida.")
