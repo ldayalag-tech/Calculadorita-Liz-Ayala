e605259b27eaf6cbf4dc577e1038d4a4bbacd3ff
ldayalag-tech <ldayalag@gmail.com>
2026-07-22 15:43:06 -0500

    Update calculadora.py
    AGREGAR LA RESTA
    LIZ AYALA

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,19 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+## CALCULADORA - VERSIÓN 2
+# Autor(a): liz ayala
 # ===========================================
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n 1.Suma")
+print( 2.Restar")
 
-print("Resultado:", suma)
+opción = input("\n Seleccione una opción: ")
+
+if opción == "1":
+  print("\n La suma es:", num1 + num2)
+
+elif opción == "2":
+  print("\n La resta es :", num1 - num2)
+
+else:
+  print("\n Opción no valida.")
